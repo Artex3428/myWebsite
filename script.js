432c3a3d5e101dd232c470c90ad2bc3cc1331440
artex050214 <132235923+artex050214@users.noreply.github.com>
2024-11-05 00:38:33 +0100

    Corrected spelling mistakes, fixed mailto, added logo link to the site

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Copyright date */
 const currentDate = new Date();
-const year = currentDate.getFullYear();
+const year = currentDate.getFullYear().toString().slice(-2);
 const month = String(currentDate.getMonth() + 1).padStart(2, '0');
 const day = String(currentDate.getDate()).padStart(2, '0');
 
